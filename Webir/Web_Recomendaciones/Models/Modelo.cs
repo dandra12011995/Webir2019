6d60a2efc8b145d34ce5112e2130d4fa9f5419a9
Daniela Andrade <dandra12011995@gmail.com>
2019-10-29 09:17:48 -0300

    Agregando DB

---
+++ Webir/Web_Recomendaciones/Models/Modelo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Web_Recomendaciones.Models
+{
+    public class Modelo : DbContext
+    {
+        public Modelo()
+            : base("name=ModeloRecoRepo")
+        {
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+        public virtual DbSet<Recomendaciones> Recomendaciones { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+    }
+}
---
+++ Webir/Web_Recomendaciones/Models/Recomendaciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web_Recomendaciones.Models
+{
+    [Table("Recomendaciones")]
+    public class Recomendaciones
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        public string repositorio { get; set; }
+
+        public int cantidad { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+    }
+}
